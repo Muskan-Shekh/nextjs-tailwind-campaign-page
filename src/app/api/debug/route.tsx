14c91835446415f90520b0143b12a5fbe514bcca
Muskan <muskan@techbeeps.co.in>
2025-04-14 17:29:46 +0530

    updated code

--- src/app/api/debug/route.tsx
+++ src/app/api/debug/route.tsx
@@ -33,32 +33,34 @@
 //   }
 // }
 
-
-// GET 
+// GET
 // app/api/debug/route.ts
 
 import { NextRequest, NextResponse } from "next/server";
 import { getIronSession } from "iron-session";
 import { sessionOptions } from "@/app/lib/session";
-import { v4 as uuidv4 } from "uuid"; 
+import { v4 as uuidv4 } from "uuid";
 
 export async function GET(req: NextRequest) {
   const res = new NextResponse();
 
-  const session = await getIronSession(req, res, sessionOptions);
-
+  const session: any = await getIronSession(req, res, sessionOptions);
+  console.log("session", session);
   try {
     if (!session.session_id) {
-              session.session_id = uuidv4();
-            }
+      session.session_id = uuidv4();
+    }
     session.count = session.count || 0;
     session.count += 1;
     await session.save();
 
-    return NextResponse.json({
+    return NextResponse.json(
+      {
         session_id: session.session_id,
-      count: session.count,
-    }, res);
+        count: session.count,
+      },
+      res
+    );
   } catch (error: any) {
     console.error("Session error:", error);
     return NextResponse.json({ error: error.message }, { status: 500 });
